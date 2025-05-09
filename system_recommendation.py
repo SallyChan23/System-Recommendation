0cf2343b6b7e9a3cedc7a02797f252716b7742d9
Jeselyn Tania <jeselyntan01@gmail.com>
2025-05-09 16:47:37 +0700

    update

--- system_recommendation.py
+++ system_recommendation.py
@@ -14,14 +14,20 @@ df = pd.read_csv('/kaggle/input/spotify-dataset-1921-2020-160k-tracks/data.csv')
 
 # EDA
 df.info()
-print("Jumlah missing value:\n", df.isnull().sum())
+df.describe()
+print("Jumlah missing value:")
+print(df.isnull().sum())
 print("Jumlah data duplikat:", df.duplicated().sum())
+
+# Pemilihan fitur audio
 fitur_audio = [
     'danceability', 'energy', 'key', 'loudness', 'mode',
     'speechiness', 'acousticness', 'instrumentalness',
     'liveness', 'valence', 'tempo'
 ]
-print(df[fitur_audio].head())
+
+df[fitur_audio].head()
+print(df.columns.tolist())
 
 # Data Preparation
 scaler = MinMaxScaler()
@@ -41,7 +47,7 @@ similarity_df = pd.DataFrame(similarity_matrix,
 def rekomendasi_lagu(judul_lagu, df_similar, jumlah=5):
     if judul_lagu not in df_similar.columns:
         return f"Lagu '{judul_lagu}' tidak ditemukan dalam dataset."
-
+    
     similar_songs = df_similar[judul_lagu].sort_values(ascending=False).iloc[1:jumlah+1]
     hasil_df = pd.DataFrame({
         'Track Name': similar_songs.index,
@@ -49,18 +55,17 @@ def rekomendasi_lagu(judul_lagu, df_similar, jumlah=5):
     })
     return hasil_df
 
-# Fungsi Evaluasi
 def evaluasi_lagu(list_judul, df_similar):
     for judul in list_judul:
         print(f"\nJudul: {judul}")
         hasil = rekomendasi_lagu(judul, df_similar)
         if isinstance(hasil, str):
-            print("Tidak ada", hasil)
+            print("❌", hasil)
         else:
-            print("Rekomendasi lagu mirip:")q
+            print("✅ Rekomendasi lagu mirip:")
             print(hasil)
 
-# Evaluasi Sistem
+# Evaluasi
 lagu_uji = [
     'Camby Bolongo', 'Castle on a Cloud', 'Someone Like You',
     'Eso Hey Sajal Shyam Ghana Deya', 'Bohemian Rhapsody'
